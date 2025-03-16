dff6b3c304207ed48ab45380651c701e31a5eacb
HenryJKS <henryjooji30@gmail.com>
2025-03-16 02:14:18 -0300

    config env

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from app.routes.predict_route import predict_route
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'mysecretkey'
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
     # Registrar o Blueprint de predição
     app.register_blueprint(predict_route)
